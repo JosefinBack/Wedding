3d3f712d6b441cb26ac22bc7f9bb72d7fa1fb669
JosefinBack <josefin.m.mansson@gmail.com>
2025-09-28 17:15:41 +0200

    header

---
+++ Javascript/header.js
@@ -0,0 +1,13 @@
+let header = document.getElementById("header");
+let meny = document.getElementById("meny");
+
+header.innerHTML = `
+    <h2>Välkommen på bröllop mellan</h2>
+    <h1>Anna och Johan</h1>
+    <h3>9 maj 2026</h3>
+`
+
+
+meny.innerHTML = `
+
+`
