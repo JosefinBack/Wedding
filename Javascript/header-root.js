3467955761e85e4903206ad3cf6d475f88de86a1
JosefinBack <josefin.m.mansson@gmail.com>
2025-09-28 20:29:18 +0200

    bakgrund och meny

---
+++ Javascript/header-root.js
@@ -0,0 +1,22 @@
+let header = document.getElementById("header");
+let meny = document.getElementById("meny");
+
+header.innerHTML = `
+    <h2 id ="headerH2">Välkommen på bröllop mellan</h2>
+    <h1>Anna och Johan</h1>
+    <h3 id ="headerH3">9 maj 2026</h3>
+`
+
+
+meny.innerHTML = `
+    <div id = "topMeny">
+        <a href ="index.html">Välkommen</a>
+        <a href ="HTML/wedding.html">Bröllopet</a>
+        <a href ="HTML/info.html" >Info</a>
+        <a href ="HTML/housing.html">Boende</a>
+        <a href ="HTML/list.html">Gåvor</a>
+        <a href ="HTML/osa.html">O.S.A</a>
+        <a href ="HTML/toastmaster.html">Tal & Toastmaster</a>
+        <a href ="HTML/contact.html">Kontakt</a>
+    </div>
+`
